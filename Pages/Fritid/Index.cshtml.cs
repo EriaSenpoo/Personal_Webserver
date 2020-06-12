4bf3d005d790e8954f5738921b6fb4d450d710e9
DovahViking <DovahViking@hotmail.com>
2020-06-12 11:40:07 +0200

    Layout issues and creating new issues

---
+++ Pages/Fritid/Index.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Personal_Website.Pages.Fritid
+{
+    public class IndexModel : PageModel
+    {
+        public string aaa { get; set; }
+        public void OnGet()
+        {
+            aaa = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+        }
+    }
+}
