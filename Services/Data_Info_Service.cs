6a591bad455f342272fa92e87425dfaa6561d6e3
DovahViking <DovahViking@hotmail.com>
2020-06-24 08:45:52 +0200

    .gitignore test

--- Services/Data_Info_Service.cs
+++ Services/Data_Info_Service.cs
@@ -27,18 +27,6 @@ namespace Personal_Website.Services
         {
             get { return Path.Combine(web_host_environment.WebRootPath, "data", "json_data.json"); }
         }
-
-        //public Tuple<List<string>, List<JToken>> get_data_om_mig_info_data()
-        //{
-        //    StreamReader reader = File.OpenText(json_file_name);
-        //    JToken data = JToken.Parse(reader.ReadToEnd());
-        //    JObject om_mig_info = data["om_mig_info"].Value<JObject>();
-        //    List<string> subjects = om_mig_info.Properties().Select(property => property.Name).ToList();
-        //    List<JToken> text = om_mig_info.Properties().Select(property => property.Value).ToList();
-
-        //    Tuple<List<string>, List<JToken>> om_mig_data = new Tuple<List<string>, List<JToken>>(subjects, text);
-        //    return om_mig_data;
-        //}
         public List<Om_mig_text_object> get_data_om_mig_info_data()
         {
             StreamReader reader = File.OpenText(json_file_name);
