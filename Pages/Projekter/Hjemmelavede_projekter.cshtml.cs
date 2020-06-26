958a477b6f9be268a44535f5a3dfdccb9f3162e8
DovahViking <DovahViking@hotmail.com>
2020-06-26 12:44:35 +0200

    added home-made projects, json data, new razor page

--- Pages/Projekter/Hjemmelavede_projekter.cshtml.cs
+++ Pages/Projekter/Hjemmelavede_projekter.cshtml.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Personal_Website.Models;
@@ -11,22 +9,22 @@ using Personal_Website.Services;
 
 namespace Personal_Website.Pages.Projekter
 {
-    public class IndexModel : PageModel
+    public class Hjemmelavede_projekterModel : PageModel
     {
         public List<Data_text_object> list = new List<Data_text_object>();
         public Data_Info_Service Service_text { get; set; }
-        public string banner { get; set; }
+        //public string banner { get; set; }
 
-        public IndexModel(Data_Info_Service Service_text)
+        public Hjemmelavede_projekterModel(Data_Info_Service Service_text)
         {
             this.Service_text = Service_text;
         }
 
         public void OnGet()
         {
-            banner = Url.Content("~/images/projekter_banner.png");
-            ViewData["Banner"] = banner;
-            list = Service_text.get_data("projekter");
+            //banner = Url.Content("~/images/projekter_banner.png");
+            //ViewData["Banner"] = banner;
+            list = Service_text.get_data("hjemmelavede_projekter");
         }
     }
 }
